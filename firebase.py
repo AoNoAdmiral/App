21e23e210ade4cc91ab944467e8a55429e4dd768
BaoKha-S <khabaotran123@gmail.com>
2022-04-02 09:53:12 +0700

    Update firebase.py

--- firebase.py
+++ firebase.py
@@ -1,27 +1,26 @@
-from dataclasses import dataclass
-import email
+
 import pyrebase
 
 
-firebaseConfig = {
-  'apiKey': "AIzaSyBvSDvuuBcheDg6fZUpi30Il-MUogLKwV4",
-  'authDomain': "chill-2ddd1.firebaseapp.com",
-  'databaseURL': "https://chill-2ddd1-default-rtdb.firebaseio.com",
-  'projectId': "chill-2ddd1",
-  'databaseURL': "https://chill-2ddd1-default-rtdb.firebaseio.com/",
-  'storageBucket': "chill-2ddd1.appspot.com",
-  'messagingSenderId': "62414238957",
-  'appId': "1:62414238957:web:04d88c13d1ac0510a808e4",
-  'measurementId': "G-ZG9Z0XL8MW"
+config = {
+  "apiKey": "AIzaSyBvSDvuuBcheDg6fZUpi30Il-MUogLKwV4",
+  "authDomain": "chill-2ddd1.firebaseapp.com",
+  "databaseURL": "https://chill-2ddd1-default-rtdb.firebaseio.com",
+  "projectId": "chill-2ddd1",
+  "databaseURL": "https://chill-2ddd1-default-rtdb.firebaseio.com/",
+  "storageBucket": "chill-2ddd1.appspot.com",
+  "messagingSenderId": "62414238957",
+  "appId": "1:62414238957:web:04d88c13d1ac0510a808e4",
+  "measurementId": "G-ZG9Z0XL8MW"
 }
 
-firebase= pyrebase.initialize_app(firebaseConfig)
+firebase = pyrebase.initialize_app(config)
 
-database= firebase.database()
+db = firebase.database()
 
 data = {"Age":21, "Name": "Kha", "Staff": True}
 
 #----------------------------------------
 #create data
 
-database.push(data)
+db.push(data)
