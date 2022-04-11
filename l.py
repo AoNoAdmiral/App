1c467b8bfbebe8c4b568703294c4dce55840b6b9
AoNoAdmiral <90148066+AoNoAdmiral@users.noreply.github.com>
2022-04-12 00:24:57 +0700

    X
    A

--- l.py
+++ l.py
@@ -2,18 +2,32 @@ import serial
 import paho.mqtt.client as mqtt
 import time
 import json
+import threading
 
 THINGSBOARD_HOST = "demo.thingsboard.io"
 ACCESS_TOKEN = 'LyoSMl8n9Yoki1fBpJoj'   
-request = {"method": "gettelemetry", "params": {}}
+request = {"method": "check1", "params": {}}
+
+
+def update():
+    while True:
+        client.publish('v1/devices/me/rpc/request/1',json.dumps(request), 1)
+        time.sleep(100)
 
 def on_connect(client, userdata, flags, rc):
     print("rc code:", rc)
-    client.subscribe('v1/devices/me/rpc/response/+')
+    client.subscribe('v1/devices/me/rpc/response/1')
+    
 
 def on_message(client, userdata, msg):
     print('Topic: ' + msg.topic + '\nMessage: ' + msg.payload.decode("utf-8"))
+    r =   json.loads(msg.payload.decode("utf-8"))
+    #dk
+    # r luu dk tuoi tu dong. No print message khi chay nen may biet dang data
+    # ko biet nhan t
+    watering = 1
 
+watering = 0
 client = mqtt.Client()    
 client.loop_start()
 client.on_connect = on_connect
@@ -22,17 +36,24 @@ client.on_message = on_message
 client.username_pw_set(ACCESS_TOKEN)
 client.connect(THINGSBOARD_HOST, 1883,60)
 
+threading.Thread(target=update).start()
+
+#time lay bang
+import datetime
+# hour = datetime.now().hour() -> Nho cong 7 roi mod 24 ( no lay h 0 va minh h bay)
+# min = datetime.now().minute()
 
 ser = serial.Serial('COM4', 19200)
-# while  True:
-#     while ser.in_waiting:
-#         data_in = ser.readline()
-#         print(data_in)
-#         client.publish('v1/devices/me/telemetry', json.dumps(data_in), 1)
+while  True:
+    # lay data
+    if watering:
+        #tuoi cay
+        pass
+    else:
+        #show bang
+        pass
 
-while True:
-    client.publish('v1/devices/me/rpc/request/1',json.dumps(request), 1)
-    time.sleep(5)
+
         
 
 
