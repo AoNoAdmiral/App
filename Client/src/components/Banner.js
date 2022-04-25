93cbb8c70c18b7e8a405b429fe6bdf71fe2e36ce
BaoKha-S <khabaotran123@gmail.com>
2022-04-25 18:33:05 +0700

    kha

--- Client/src/components/Banner.js
+++ Client/src/components/Banner.js
@@ -4,18 +4,6 @@ function Banner() {
     return (
         <section className="banner-container">
 
-<<<<<<< Updated upstream
-<div class="sale1">
-          <div class="card">
-            <img src="https://images.pexels.com/photos/4590488/pexels-photo-4590488.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" class="card-imgage"/>            
-              <div class="sale-user-left">
-                <h4 class="sale-user-name">LIME</h4>
-                <p class="sale-user-detail">There are several species of citrus trees whose fruits are called limes</p>
-              </div>
-              <div class="sale-user-right">
-                <span class="sale-user-after">60.000VND</span>
-                <span class="sale-user-before">90.000VND</span>
-=======
           <div class="sale">
           <div class="sale1">
           <div class="card">
@@ -26,7 +14,6 @@ function Banner() {
 
               </div>
               <div class="sale-user-right">
->>>>>>> Stashed changes
                 <div class="banner__sale-buynow">
                   <a href="#" class="header__menu-link btn btn--border btn--rounded">
                   Buy Now
@@ -37,21 +24,12 @@ function Banner() {
         </div>
         <div class="sale2">
           <div class="card">
-<<<<<<< Updated upstream
-            <img src="https://images.pexels.com/photos/4590488/pexels-photo-4590488.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" class="card-imgage"/>            
-=======
             <img src="https://images.pexels.com/photos/736779/pexels-photo-736779.jpeg?cs=srgb&dl=pexels-iconcom-736779.jpg&fm=jpg" alt="" class="card-imgage"/>            
->>>>>>> Stashed changes
               <div class="sale-user-left">
                 <h4 class="sale-user-name">LIME</h4>
                 <p class="sale-user-detail">There are several species of citrus trees whose fruits are called limes</p>
               </div>
               <div class="sale-user-right">
-<<<<<<< Updated upstream
-                <span class="sale-user-after">60.000VND</span>
-                <span class="sale-user-before">90.000VND</span>
-=======
->>>>>>> Stashed changes
                 <div class="banner__sale-buynow">
                   <a href="#" class="header__menu-link btn btn--border btn--rounded">
                   Buy Now
@@ -60,10 +38,7 @@ function Banner() {
               </div>            
           </div>
         </div>
-<<<<<<< Updated upstream
-=======
           </div>
->>>>>>> Stashed changes
 
 
         </section>
